930eca62c88b8d3cf207ddbd87c888c17acaf61f
Carsten Sandker <carsten.sandker@googlemail.com>
2014-02-14 01:24:05 +0100

    Fixed: marker Position after draged not changed, heading not calculated

--- admin/apis/edit_map_api.php
+++ admin/apis/edit_map_api.php
@@ -11,11 +11,11 @@ $dblk = connect();
 // Action: saveNeighbour [POST]
 if(isset($_POST['saveNeighbour'])){
   // DROP all Neighbour Connections
-  $photoId = mysql_escape_string($_POST['saveNeighboursFor']['photoId']);
+  $panoramaId = mysql_escape_string($_POST['saveNeighboursFor']['panoramaId']);
   // Delete straight connections
-  sql("DELETE FROM neighbour WHERE panorama = $photoId ");
+  sql("DELETE FROM neighbour WHERE panorama = $panoramaId ");
   // Delete reverted Connection if exists
-  sql("DELETE FROM neighbour WHERE neighbour = $photoId");
+  sql("DELETE FROM neighbour WHERE neighbour = $panoramaId");
   // Iterate through given Neighbours
   foreach ($_POST['saveNeighboursFor']['neighbours'] as $neighbourId => $heading) {
     // Save Neighbour Connection
@@ -23,23 +23,23 @@ if(isset($_POST['saveNeighbour'])){
     $heading = mysql_escape_string($heading);
     // Insert reverted Connection
     $revertedHeading = ( $heading + 180 ) % 360;
-    sql("INSERT INTO neighbour (panorama, neighbour, heading) VALUES ($neighbourId, $photoId, $revertedHeading)");
+    sql("INSERT INTO neighbour (panorama, neighbour, heading) VALUES ($neighbourId, $panoramaId, $revertedHeading)");
     // Save straight connection
-    sql("INSERT INTO neighbour (panorama, neighbour, heading) VALUES ($photoId, $neighbourId, $heading)");
+    sql("INSERT INTO neighbour (panorama, neighbour, heading) VALUES ($panoramaId, $neighbourId, $heading)");
   }
 }
 
 // Action: updateHeading [POST]
 if(isset($_POST['updateHeading'])){
   // escape Parameters
-  $photoId = mysql_escape_string($_POST['photoId']);
+  $panoramaId = mysql_escape_string($_POST['panoramaId']);
   $neighbourId = mysql_escape_string($_POST['neighbourId']);
   $heading = mysql_escape_string($_POST['heading']);
   // update DB
-  // Heading: photoId -> neihbour
-  sql("UPDATE neighbour SET heading = $heading WHERE panorama = $photoId AND neighbour = $neighbourId");
-  // revertedHeading: neihbour -> photoId
+  // Heading: panoramaId -> neihbour
+  sql("UPDATE neighbour SET heading = $heading WHERE panorama = $panoramaId AND neighbour = $neighbourId");
+  // revertedHeading: neihbour -> panoramaId
   $revertedHeading = ( $heading + 180 ) % 360;
-  sql("UPDATE neighbour SET heading = $revertedHeading WHERE panorama = $neighbourId AND neighbour = $photoId");
+  sql("UPDATE neighbour SET heading = $revertedHeading WHERE panorama = $neighbourId AND neighbour = $panoramaId");
 }
 ?>
