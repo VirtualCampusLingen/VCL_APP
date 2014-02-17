2404f52e0e883a9de23ae0f646413dd879af5df1
Jannik Fangmann <jannik.fangmann@me.com>
2014-02-17 02:44:04 +0100

    Points of interest as media object

--- _poi_modal.php
+++ _poi_modal.php
@@ -1,3 +1,17 @@
+<?php
+$DOCUMENT_ROOT = dirname(__FILE__);
+
+$tools_dir = $DOCUMENT_ROOT . "/admin/tools/";
+include_once($tools_dir . "connect.php");
+include_once($tools_dir . "sql.php");
+include_once($tools_dir . "log.php");
+$dblk = connect();
+
+$error = 0;
+error_reporting(null);
+
+?>
+
 <div id="poi_modal" class="modal fade">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -6,7 +20,26 @@
         <h4 class="modal-title">Wo möchtest du hin?</h4>
       </div>
       <div class="modal-body">
-        <p>One fine body&hellip;</p>
+      	<ul class="media-list" style="height: 300px; overflow: auto">
+	        <?php
+	        	$sql_poi = sql("SELECT name, description, panorama FROM poi ORDER BY name ASC");
+				$i = 0;
+				while($row = mysql_fetch_assoc($sql_poi)){
+					$poi_name = $row['name'];
+					$poi_description = $row['description'];
+					$poi_panorama = $row['panorama'];
+					
+					echo '<li class="media">';
+					echo '<a class="pull-left" href="#"><img class="media-object" src="/admin/assets/img/poi_example.png" alt="' . $poi_name . '"></a>';
+					echo '<div class="media-body">';
+					echo '<h4 class="media-heading">' . $poi_name . '</h4>';
+					echo '<p>' . $poi_description . '</p>';
+					echo '</li>';					
+					
+					$i++;
+				}
+	        ?>
+        </ul>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>
